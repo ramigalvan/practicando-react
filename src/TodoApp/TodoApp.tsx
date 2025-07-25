882be549814435866d02ee205d3b389a6d5cd2fb
ramiro galvan <ramiro_coronel333@hotmail.com>
2025-07-25 12:48:09 -0300

    feat(TodoApp): items guardados en localstorage

--- src/TodoApp/TodoApp.tsx
+++ src/TodoApp/TodoApp.tsx
@@ -1,17 +1,40 @@
 import { useEffect, useState, type FormEvent } from "react"
 
+//TODO: modularizar
+//usa modals para el boton de modificar
+
+interface Task {
+    id: string;
+    text: string;
+    completed: boolean;
+}
 function TodoApp() {
-    const [task, setTask] = useState<string>("");
-    const [tasks, setTasks] = useState<string[]>(() => {
+    const [task, setTask] = useState<Task>({
+        completed: false,
+        text: "",
+        id: ""
+    });
+
+
+    const [tasks, setTasks] = useState<Task[]>(() => {
         const stored = localStorage.getItem("tasks")
         return stored ? JSON.parse(stored) : [];
     });
 
     const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        if (!task.trim()) return;
-        setTasks([...tasks, task]);
-        setTask("");
+        if (!task.text.trim()) return;
+
+        const newTask = {
+            ...task,
+            id: crypto.randomUUID()
+        }
+        setTasks([...tasks, newTask]);
+        setTask({
+            id: "",
+            text: "",
+            completed: false
+        });
     }
 
     useEffect(() => {
@@ -26,9 +49,9 @@ function TodoApp() {
                 <input
                     placeholder="Estudiar hoisting de JS..."
                     type="text"
-                    value={task}
+                    value={task.text}
                     required
-                    onChange={(e) => setTask(e.target.value)}
+                    onChange={(e) => setTask({...task, text: e.target.value})}
                     id="input-tarea"
                 />
                 <button type="submit">Agregar</button>
@@ -36,8 +59,19 @@ function TodoApp() {
             <h3>Lista de tareas</h3>
             <ul>
                 {
-                    tasks.map((value, index) =>
-                        <li key={index}>{value}</li>
+                    tasks.map(({text, id}) =>
+                        <li style={{
+                            marginBottom: "1rem",
+                            display : "flex", 
+                            flexDirection: "column",
+                            alignItems: "flex-start",
+                            }} 
+                            key={id}>
+                            <span>{text}</span>
+                            <button>hacer</button>
+                            <button>completar</button>
+                            <button>modificar</button>
+                        </li>
                     )
                 }
             </ul>
